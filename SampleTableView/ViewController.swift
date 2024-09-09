2451388cedc9acbdfc629619017982666ee554f4
serijun1214 <serijun.1214+github@gmail.com>
2024-09-10 05:52:40 +0900

    リストを使った実装

--- SampleTableView/ViewController.swift
+++ SampleTableView/ViewController.swift
@@ -7,13 +7,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let fruits = ["りんご", "みかん", "ぶどう", "レモン", "スイカ"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return fruits.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel!.text = fruits[indexPath.row]
+        return cell
+    }
 
 }
 
